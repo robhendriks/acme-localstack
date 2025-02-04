c8877e2d53fc0ff2a00084de1752034545512172
robhendriks <rob.hendriks@iodigital.com>
2025-02-04 21:37:57 +0100

    fix: bug in construct lib

--- infra/lib/util/construct.ts
+++ infra/lib/util/construct.ts
@@ -1,7 +1,6 @@
 import { Node } from "constructs";
-import kebabCase from "kebab-case";
 
 export function generateName(node: Node, ...paths: string[]): string {
-  const segments = [...node.path.split("/"), ...paths];
-  return segments.map((s) => kebabCase(s, false)).join("-");
+  const segments = [...node.path.split(/\//), ...paths];
+  return segments.join("-");
 }
